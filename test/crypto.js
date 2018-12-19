0721a3cab2e6c0ab34338fe307cf188196f13073
eloven <guobin201314@gmail.com>
2018-12-19 09:24:50 +0800

    加密模块

---
+++ test/crypto.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto');
+const secret = 'abcdefg';
+/**
+ * 加密模块
+ * @type {string}
+ */
+const hash = crypto.createHmac('sha256', secret)
+    .update('I love cupcakes')
+    .digest('hex');
+console.log(hash);
